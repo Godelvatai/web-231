e074c06d1e041e9940cd37add47cfacb4fd4d159
Godelvatai <godelvatai@gmail.com>
2024-01-28 23:20:39 -0500

    Homework Upload
    Homework Upload

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,23 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Dustin Craven
+      Date:   1/21/2024
 
       Filename: project02-02.js
  */
- 
+
+function verifyForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+
+  console.log("EVENT LISTENED")
+  if(name && email && phone) {
+    window.alert("Thank you!");
+  } else {
+    window.alert("Please fill in all fields.")
+  }
+}
+
+document.getElementById("submit").addEventListener("click", verifyForm);
