af30a40f312e06df63e66e581a368382581f1b58
Godelvatai <godelvatai@gmail.com>
2024-02-26 00:43:20 -0500

    Homework Upload
    Homework Upload

--- week-7/project05-01/project05-01_txt.js
+++ week-7/project05-01/project05-01_txt.js
@@ -4,15 +4,17 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Dustin Craven
+      Date:   2/25/24
 
       Filename: project05-01.js
 */
 
+"use strict";
+
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
-const quizTime = 20;
+const quizTime = 90;
 const correctAnswers = ["10", "4", "-6", "5", "-7"];
 
 // Elements in the quiz page
@@ -26,7 +28,40 @@ let timeLeft = quizTime;
 
 // Declare the ID for timed commands
 // and the node list for questions
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
 
+//countdown function to update quiz clock
+function countdown() {
+  if(timeLeft === 0) {
+    //clear the timer interval
+    clearInterval(timeID);
+
+    //Read number of correct answers and display it to user
+    let totalCorrect = checkAnswers();
+    if(totalCorrect === correctAnswers.length) {
+      alert("You've answered 100% of the questions correctly. Congratulations!")
+    } else {
+      alert("You answered "+totalCorrect+" out of "+correctAnswers.length+" correctly.")
+      //Reset quiz
+      timeLeft = quizTime;
+      quizClock.value = timeLeft;
+      document.getElementById("overlay").className = "hidequiz";
+    }
+  } else {
+    timeLeft -= 1;
+    quizClock.value = timeLeft;
+  }
+}
+
+//Add onclick event handler to startQuiz object
+document.getElementById("startquiz").addEventListener("click", function() {
+  //Change class of overlay to show quiz to user
+  document.getElementById("overlay").className = "showquiz";
+
+  //Run countdown function on 1000 millisecond intervals
+  timeID = setInterval(countdown, 1000);
+});
 
 
 
@@ -50,14 +85,14 @@ let timeLeft = quizTime;
 /*------------- Function to check the student answers ----------------*/
 function checkAnswers() {
    let correctCount = 0;
-   
+
    for (let i = 0; i < questionList.length; i++) {
       if (questionList[i].value === correctAnswers[i]) {
          correctCount++;
          questionList[i].className = "";
       } else {
          questionList[i].className = "wronganswer";
-      }      
+      }
    }
    return correctCount;
 }
