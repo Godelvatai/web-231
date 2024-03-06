39a33133a33ad05e9dca9f557ae13068cecaddff
Dustin Craven <godelvatai@gmail.com>
2024-03-06 13:01:08 -0500

    Homework 9 Upload
    Homework 9 Upload

--- week-9/project07-01/project07-01_txt.js
+++ week-9/project07-01/project07-01_txt.js
@@ -1,20 +1,47 @@
-"use strict";
 /*    JavaScript 7th Edition
       Chapter 7
       Project 07-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Dustin Craven
+      Date:   3/10/2024
 
       Filename: project07-01.js
 */
+"use strict";
 
+//Declare and initialize variable pointing to form object
 let signupForm = document.getElementById("signup");
 
-signupForm.addEventListener("submit", function(e) { 
+//Event listener to prevent form from submitting until after the entered password is validated
+signupForm.addEventListener("submit", function(e) {
+   //Declare and initialize variables to get the value of the password input and a pointer to the error message feedback ID
    let pwd = document.getElementById("pwd").value;
    let feedback = document.getElementById("feedback");
 
+   //Prevent form from submitting when submit button is clicked
+   e.preventDefault();
+
+   //Declare and itialize variables storing regular expressions to use for password validation
+   let regex1 = /[A-Z]/;
+   let regex2 = /[0-9]/;
+   let regex3 = /[!\$#%]/;
+
+   //Change text of feedback when the password entered doesn't meet the required parameters and sumbit the form when the entered password is valid
+   if(pwd.length < 8) {
+    feedback.textContent = "Your password must be at least 8 characters."
+   }
+   else if(!regex1.test(pwd)) {
+    feedback.textContent = "Your password must include an uppercase letter.";
+   }
+   else if(!regex2.test(pwd)) {
+    feedback.textContent = "Your password must include a number.";
+   }
+   else if(!regex3.test(pwd)) {
+    feedback.textContent = "Your password must include on of the following: !$#%"
+   }
+   else {
+    document.forms.signup.submit();
+   }
 }
 );
